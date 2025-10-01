9c8b904f363a60eb534136125aabc0a3843756fc
aiuc <aiuc@gft.com>
2025-10-01 17:49:21 -0500

    initial commit

---
+++ src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+package com.scalesec.vulnado;
+import org.junit.runner.RunWith;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner;
+@SpringBootTest
+public class VulnadoApplicationTests {
+public class VulnadoApplicationTests {
+    @Test
+@RunWith(SpringRunner.class)
+    @Test
+    public void contextLoads() {
+    public void contextLoads() {
+    }
+public class VulnadoApplicationTests {
+}
+
+}
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
